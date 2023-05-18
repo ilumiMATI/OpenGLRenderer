7ef3e4e23d11416700e1ee4ccdefcd70df764d59
ilumiMATI <mati98848@gmail.com>
2023-05-18 17:31:56 +0200

    imgui added, MVP basics added

--- OpenGLRenderer/res/shaders/Basic.shader
+++ OpenGLRenderer/res/shaders/Basic.shader
@@ -9,17 +9,18 @@ uniform mat4 u_MVP;
 
 void main()
 {
-    float multx = 0.25 * sin(u_Time) + 1.0;
-    float multy = 0.25 * sin(u_Time*3) + 1.0;
-    float posx = position.x * 640 *multx;
-    float posy = position.y * 480 *multy;
-    float alpha = atan(posy,posx);
-    float mag = sqrt(pow(posy,2) + pow(posx,2));
+    vec4 pos = u_MVP * position;
+    //float multx = 0.25 * sin(u_Time) + 1.0;
+    //float multy = 0.25 * sin(u_Time) + 1.0;
+    //float posx = pos.x * 640 *multx;
+    //float posy = pos.y * 480 *multy;
+    //float alpha = atan(posy,posx);
+    //float mag = sqrt(pow(posy,2) + pow(posx,2));
 
-    posx = mag * cos(u_Time + alpha) / 640.0;
-    posy = mag * sin(u_Time + alpha) / 480.0;
+    //posx = mag * cos(u_Time + alpha) / 640.0;
+    //posy = mag * sin(u_Time + alpha) / 480.0;
 
-    gl_Position = u_MVP * vec4(posx,posy, 0.0, 1.0);
+    gl_Position = vec4(pos.x,pos.y, 0.0, 1.0);
 }
 
 #shader fragment
--- OpenGLRenderer/src/Application.cpp
+++ OpenGLRenderer/src/Application.cpp
@@ -12,6 +12,11 @@
 
 #include "glm/glm.hpp"
 #include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_transform.hpp>
+
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
 
 int main(void)
 {
@@ -27,7 +32,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -50,10 +55,10 @@ int main(void)
     {
 
         float positions[] = {
-            -0.5f, -0.5f,
-             0.5f, -0.5f,
-             0.5f,  0.5f,
-            -0.5f,  0.5f,
+            400.0f - 200, 300.0f - 200,
+            400.0f + 200, 300.0f - 200,
+            400.0f + 200, 300.0f + 200,
+            400.0f - 200, 300.0f + 200,
         };
         unsigned int indices[] = {
             0, 1, 2,
@@ -65,15 +70,22 @@ int main(void)
         VertexBufferLayout layout;
         layout.Push<float>(2);
         va.AddBuffer(vb, layout);
-        IndexBuffer ib(indices, 6);
 
-        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+        IndexBuffer ib(indices, 3);
+        IndexBuffer ib2(indices + 3, 3);
+
+        glm::vec3 translation = glm::vec3(0, 0, 0);
+
+        glm::mat4 proj = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, -1.0f, 1.0f);
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+        glm::mat4 model = glm::translate(glm::mat4(1.0f),translation);
+        glm::mat4 mvp = proj * view * model;
 
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 1.0, 0.5, 0.25, 1.0);
         shader.SetUniform1f("u_Time", 0.0f);
-        shader.SetUniformMat4f("u_MVP", proj);
+        shader.SetUniformMat4f("u_MVP", mvp);
 
         // unbinding
         va.Unbind();
@@ -83,21 +95,41 @@ int main(void)
 
         Renderer renderer;
 
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui::StyleColorsDark();
+        ImGui_ImplOpenGL3_Init((char*)glGetString(330));
+
         float t = 0.0f;
-        float dt = 0.015f;
+        float dt = 0.00015f;
         float r = 1.0;
         float increment = 0.008f;
+
+        bool show_demo_window = false;
+        bool show_another_window = false;
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
             renderer.Clear();
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
 
             shader.Bind();
-            shader.SetUniform4f("u_Color", r, 0.5, 0.25, 1.0);
+            shader.SetUniform4f("u_Color", 1.0, 0.5, 0.25, 1.0);
+
             shader.SetUniform1f("u_Time", t);
+            //model = glm::rotate(model, t, glm::vec3(0, 1, 0));
+            model = glm::translate(glm::mat4(1.0f), translation);
+            glm::mat4 mvp = proj * view * model;
+            shader.SetUniformMat4f("u_MVP", mvp);
 
             renderer.Draw(va, ib, shader);
+            shader.SetUniform4f("u_Color", 0.0, 0.5, 0.25, 1.0);
+            renderer.Draw(va, ib2, shader);
 
             if (r >= 1.0f)
                 increment = -increment;
@@ -105,8 +137,39 @@ int main(void)
                 increment = -increment;
 
             r += increment;
-            t += 0.01f * sin(10 * (t + dt)) + dt;
+            t += /*0.01f * sin(10 * (t + dt)) +*/ dt;
+            // Start the Dear ImGui frame
 
+            static float f = 0.0f;
+            static int counter = 0;
+
+            if (show_demo_window)
+                ImGui::ShowDemoWindow(&show_demo_window);
+
+            // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+            {
+                static float f = 0.0f;
+                static int counter = 0;
+
+                ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+                ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+                ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+                ImGui::Checkbox("Another Window", &show_another_window);
+
+                ImGui::SliderFloat3("Translation", &translation.x, -400.0f, 400.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+                ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+                if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                    counter++;
+                ImGui::SameLine();
+                ImGui::Text("counter = %d", counter);
+
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+                ImGui::End();
+            }
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
@@ -115,6 +178,10 @@ int main(void)
             glfwPollEvents();
         }
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
     glfwTerminate();
     return 0;
 }
